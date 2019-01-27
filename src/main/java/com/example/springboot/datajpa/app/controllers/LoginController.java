27956f65e2149edc31ca2f96f34ad129a064ab72
Marco Gordillo <marcopgordillo@gmail.com>
2019-01-27 09:12:15 -0500

    Validado mensaje de error en Login Controller

--- src/main/java/com/example/springboot/datajpa/app/controllers/LoginController.java
+++ src/main/java/com/example/springboot/datajpa/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.example.springboot.datajpa.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,11 +12,15 @@ import java.security.Principal;
 public class LoginController {
 
   @GetMapping("/login")
-  public String login(Model model, Principal principal, RedirectAttributes flash) {
+  public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes flash) {
 
     if (principal != null) {
       flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
       return "redirect:/";
+    }
+
+    if (error != null) {
+      model.addAttribute("error", "Usuario o contraseña incorrecta, por favor vuelva a intentar!");
     }
 
     model.addAttribute("titulo", "Por favor inicie sesión");
