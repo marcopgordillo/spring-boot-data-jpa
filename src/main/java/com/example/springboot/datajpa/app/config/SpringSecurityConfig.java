6c78a1f511c5633d4b82248e312bc0bb912e3e9e
Marco Gordillo <marcopgordillo@gmail.com>
2019-01-27 08:16:40 -0500

    Anadido en una sola linea por roles las ACLs

--- src/main/java/com/example/springboot/datajpa/app/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springboot/datajpa/app/config/SpringSecurityConfig.java
@@ -13,11 +13,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-            .antMatchers("/ver/**").hasAnyRole("USER")
-            .antMatchers("/uploads/**").hasAnyRole("USER")
-            .antMatchers("/form/**").hasAnyRole("ADMIN")
-            .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-            .antMatchers("/factura/**").hasAnyRole("ADMIN")
+            .antMatchers("/ver/**", "/uploads/**").hasAnyRole("USER")
+            .antMatchers("/form/**", "/eliminar/**", "/factura/**").hasAnyRole("ADMIN")
             .anyRequest().authenticated();
   }
 
