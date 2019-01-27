bb2accc98cd17e4c4bab5ced7c8668e05bcf97ae
Marco Gordillo <marcopgordillo@gmail.com>
2019-01-27 11:09:02 -0500

    Implementando la clase Success Handler para personalizar el mensaje de login

--- src/main/java/com/example/springboot/datajpa/app/config/SpringSecurityConfig.java
+++ src/main/java/com/example/springboot/datajpa/app/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springboot.datajpa.app.config;
 
+import com.example.springboot.datajpa.app.auth.handler.LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,6 +13,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+  private final LoginSuccessHandler loginSuccessHandler;
+
+  public SpringSecurityConfig(LoginSuccessHandler loginSuccessHandler) {
+    this.loginSuccessHandler = loginSuccessHandler;
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/webjars/**", "/listar").permitAll()
@@ -19,7 +26,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/form/**", "/eliminar/**", "/factura/**").hasAnyRole("ADMIN")
             .anyRequest().authenticated()
             .and()
-            .formLogin().loginPage("/login").permitAll()
+              .formLogin()
+                .successHandler(loginSuccessHandler)
+                .loginPage("/login")
+                .permitAll()
             .and()
             .logout().permitAll()
             .and()
