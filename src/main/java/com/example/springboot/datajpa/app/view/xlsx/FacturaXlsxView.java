e0797fc001e6a2026e8614d0c58d10768de2f438
marcopgordillo <marcopgordillo@gmail.com>
2019-01-29 10:09:59 -0500

    Implementada la clase de vista FacturaXlsxView para la factura

--- src/main/java/com/example/springboot/datajpa/app/view/xlsx/FacturaXlsxView.java
+++ src/main/java/com/example/springboot/datajpa/app/view/xlsx/FacturaXlsxView.java
@@ -1,5 +1,9 @@
 package com.example.springboot.datajpa.app.view.xlsx;
 
+import com.example.springboot.datajpa.app.models.entity.Factura;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
@@ -12,8 +16,28 @@ import java.util.Map;
 public class FacturaXlsxView extends AbstractXlsxView {
 
     @Override
-    protected void buildExcelDocument(Map<String, Object> map, Workbook workbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+    protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 
+    Factura factura = (Factura) model.get("factura");
+
+        Sheet sheet = workbook.createSheet("Factura Spring " + factura.getId());
+
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("Datos del Cliente");
+
+        row = sheet.createRow(1);
+        cell = row.createCell(0);
+        cell.setCellValue(factura.getCliente().toString());
+
+        row = sheet.createRow(2);
+        cell = row.createCell(0);
+        cell.setCellValue(factura.getCliente().getEmail());
+
+        sheet.createRow(4).createCell(0).setCellValue("Datos de la Factura");
+        sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
+        sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
+        sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
 
 
     }
