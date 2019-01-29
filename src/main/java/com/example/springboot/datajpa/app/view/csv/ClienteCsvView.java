160033be4915fd846c466eff5550c7a9db8bd1a0
marcopgordillo <marcopgordillo@gmail.com>
2019-01-29 15:24:22 -0500

    Implementando la clase de vista ClienteCsvView y Link en la vista listar clientes

--- src/main/java/com/example/springboot/datajpa/app/view/csv/ClienteCsvView.java
+++ src/main/java/com/example/springboot/datajpa/app/view/csv/ClienteCsvView.java
@@ -1,7 +1,12 @@
 package com.example.springboot.datajpa.app.view.csv;
 
+import com.example.springboot.datajpa.app.models.entity.Cliente;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,9 +25,25 @@ public class ClienteCsvView extends AbstractView {
         return true;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    protected void renderMergedOutputModel(Map<String, Object> map, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
         response.setContentType(getContentType());
+        response.setCharacterEncoding("UTF-8");
+
+        Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+
+        ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(),  CsvPreference.EXCEL_PREFERENCE);
+
+        String[] header = {"id", "nombre", "apellido", "email", "dayOfBirth"};
+        beanWriter.writeHeader(header);
+
+        for (Cliente cliente : clientes) {
+            beanWriter.write(cliente, header);
+        }
+
+        beanWriter.close();
+
     }
 }
