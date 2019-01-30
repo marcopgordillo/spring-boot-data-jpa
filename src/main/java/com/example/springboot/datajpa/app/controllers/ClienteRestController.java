2200e52df429ea4f87f5857e3eb12f592272d751
Marco Gordillo <marcopgordillo@gmail.com>
2019-01-29 22:33:25 -0500

    Vista implicita XML y JSON usando la anotacion @RestController a nivel de la clase Controlador

---
+++ src/main/java/com/example/springboot/datajpa/app/controllers/ClienteRestController.java
@@ -0,0 +1,27 @@
+package com.example.springboot.datajpa.app.controllers;
+
+import com.example.springboot.datajpa.app.models.service.IClienteService;
+import com.example.springboot.datajpa.app.view.xml.ClienteList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteRestController {
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  private final IClienteService clienteService;
+
+  public ClienteRestController(IClienteService clienteService) {
+    this.clienteService = clienteService;
+  }
+
+  @GetMapping("/listar")
+  public ClienteList listar() {
+    return new ClienteList(clienteService.findAll());
+  }
+}
